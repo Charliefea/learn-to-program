bea543cec33bccd0f1a3f49fd255c268b02cb4bc
Charlotte <charlotte.feather@yahoo.com>
2017-04-17 12:51:51 +0100

    Chapter 5

---
+++ chap5/ex5.1.rb
@@ -0,0 +1,15 @@
+var1 = 2
+var2 = '5'
+puts var1.to_s + var2
+puts var1 + var2.to_i
+
+
+puts '15'.to_f
+puts '99.999'.to_f
+puts '99.999'.to_i
+puts " "
+puts "5 is my favourite number!".to_i
+puts "Your mama did.".to_f
+puts 'stringy'.to_s
+puts 3.to_i
+ 
---
+++ chap5/ex5.6.b.rb
@@ -0,0 +1,6 @@
+puts "Can I have your fave number?"
+number = gets.chomp.to_i
+
+betternum = number + 1
+
+puts "I think this one is better -" + betternum.to_s
